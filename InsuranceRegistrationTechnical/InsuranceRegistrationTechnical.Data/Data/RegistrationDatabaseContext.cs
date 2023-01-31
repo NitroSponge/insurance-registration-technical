1cc746fba9b2a19163477d33614395e0de850726
NitroSponge <davidalanwhite@sky.com>
2023-01-31 14:29:58 +0000

    add data project with dbcontext and user entity

---
+++ InsuranceRegistrationTechnical/InsuranceRegistrationTechnical.Data/Data/RegistrationDatabaseContext.cs
@@ -0,0 +1,10 @@
+﻿using InsuranceRegistrationTechnical.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InsuranceRegistrationTechnical.Data.Data
+{
+    public class RegistrationDatabaseContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; } = null!;
+    }
+}
---
+++ InsuranceRegistrationTechnical/InsuranceRegistrationTechnical.Data/Entities/UserEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceRegistrationTechnical.Data.Entities
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = default!;
+
+        public string Surname { get; set; } = default!;
+
+        public string PolicyReferenceNumber { get; set; } = default!;
+
+        public DateOnly? DateOfBirth { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
