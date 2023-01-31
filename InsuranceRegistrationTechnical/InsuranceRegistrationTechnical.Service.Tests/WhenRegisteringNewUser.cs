d035e2aeab3a8103dbedce610a12decf38d68e03
NitroSponge <davidalanwhite@sky.com>
2023-01-31 16:57:46 +0000

    add test skeleton

---
+++ InsuranceRegistrationTechnical/InsuranceRegistrationTechnical.Service.Tests/WhenRegisteringNewUser.cs
@@ -0,0 +1,15 @@
+namespace InsuranceRegistrationTechnical.Service.Tests;
+
+public class WhenRegisteringNewUser
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void When_registering_with_invalid_email()
+    {
+        Assert.Pass();
+    }
+}
