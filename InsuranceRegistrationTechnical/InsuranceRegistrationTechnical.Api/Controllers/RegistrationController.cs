585907b2232d1e03d92673690f55e1c4aaa8cec1
NitroSponge <davidalanwhite@sky.com>
2023-01-31 14:24:03 +0000

    initial commit

---
+++ InsuranceRegistrationTechnical/InsuranceRegistrationTechnical.Api/Controllers/RegistrationController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsuranceRegistrationTechnical.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly ILogger<RegistrationController> _logger;
+
+        public RegistrationController(ILogger<RegistrationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost(Name = "PostCreateUserRequest")]
+        public Task<int> PostCreateUserRequest(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(1);
+        }
+    }
+}
